695b5930860dfba6d9ec99e55579e407cd63d61c
marioo_ra <mariora.0013@gmail.com>
2025-02-12 16:44:25 +0100

    new(eja.e5)

---
+++ src/ent1/eja/e5/InvoiceItem.java
@@ -0,0 +1,67 @@
+package ent1.eja.e5;
+
+public class InvoiceItem {
+    // ZONA DE ATRIBUTOS
+    private String id;
+    private String desc;
+    private int qty;
+    private double unitPrice;
+
+
+    // ZONA DE METODOS
+        // Constructor
+    public InvoiceItem (String id, String desc, int qty, double unitPrice){
+        this.id = id;
+        this.desc = desc;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+        // Getter's y setter's
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+        // Otros
+    public double getTotal(){
+        return unitPrice * qty;
+    }
+
+    @Override
+    public String toString() {
+        return "InvoiceItem [{]" +
+                "id='" + id +
+                ", desc='" + desc +
+                ", qty=" + qty +
+                ", unitPrice=" + unitPrice +
+                ']';
+    }
+}
